da2b6aff3c0746509dc916f7b745ec58087439d3
Saikat Kanjilal <skanjila@Saikats-MacBook-Pro.local>
2014-03-28 18:36:14 -0700

    mahout fork with scala shell

---
+++ spark/src/test/scala/org/apache/mahout/sparkbindings/test/LoggerConfiguration.scala
@@ -0,0 +1,16 @@
+package org.apache.mahout.sparkbindings.test
+
+import org.scalatest.Suite
+import org.apache.log4j.{Level, Logger, BasicConfigurator}
+
+/**
+ * @author dmitriy
+ */
+trait LoggerConfiguration extends org.apache.mahout.test.LoggerConfiguration {
+  this: Suite =>
+
+  override protected def beforeAll(): Unit = {
+    super.beforeAll()
+    Logger.getLogger("org.apache.mahout.sparkbindings").setLevel(Level.DEBUG)
+  }
+}
